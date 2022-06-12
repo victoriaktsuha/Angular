94062f4e6a3b467b990d43c08e18b5b5365df247
Victoria K. Tsuha <32991496+vickats@users.noreply.github.com>
2022-06-12 19:18:26 -0300

    Data Binding
    String and Function Interpolation and Property Binding (HTML attribute)

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-servers',
+  selector: 'app-servers',
   //selector: '[app-servers]',
-  selector: '.app-servers',
+  //selector: '.app-servers',
   /*template: '<app-server></app-server><app-server></app-server>',*/
-  template: `
+  /* template: `
     <app-server></app-server>
-    <app-server></app-server>`,
+    <app-server></app-server>`, */
+    templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer:boolean = false;
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit(): void {
   }
