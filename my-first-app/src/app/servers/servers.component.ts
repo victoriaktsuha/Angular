739b2266577f509c570dbe95a6da98cc34108e78
Victoria K. Tsuha <32991496+vickats@users.noreply.github.com>
2022-06-23 09:32:55 -0300

    Directives - ngIf

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -15,8 +15,9 @@ export class ServersComponent implements OnInit {
   allowNewServer:boolean = false;
   serverCreationStatus:string = 'No server was created!';
   serverName = '';
-  username = '';
-  allowUsernameButton:boolean = false;
+  /* username = '';
+  allowUsernameButton:boolean = false; */
+  serverCreated:boolean = false;
 
   constructor() { 
     setTimeout(() => {
@@ -28,20 +29,21 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
   }
 
   onUpdateServerName(event: Event){
     this.serverName = (<HTMLInputElement>event.target).value;
   }
-
-  allowUsernameType(){
+  /* Assignment 2 My solution */
+  /*allowUsernameType(){
     this.allowUsernameButton = true;
   }
 
   onUsernameCreate(){
     this.username = '';
     this.allowUsernameButton = false;
-  }
+  } */
 
 }
