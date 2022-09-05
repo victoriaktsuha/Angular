506a0d701e2cc6933ba7ea3836093da39b89cad2
Victoria K. Tsuha <32991496+vickats@users.noreply.github.com>
2022-09-05 19:10:55 -0300

    Planning App

--- my-first-app/src/app/assignments/Assignment_3_Directives/assignment-3.component.ts
+++ my-first-app/src/app/assignments/Assignment_3_Directives/assignment-3.component.ts
@@ -11,6 +11,7 @@ export class Assign3Component {
 
     onToggleDetails() {
         this.showSecret = !this.showSecret;
-        this.log.push(this.log.length + 1);
+        //this.log.push(this.log.length + 1);
+        this.log.push(new Date());
     }
 }
