5351ddf9cbd01fcacd63b960a665d29a86a76dc0
vickats <victoriaktsuha@outlook.com>
2023-05-02 17:23:34 -0300

    Angular Udemy #30

--- my-ng-app/src/app/servers/servers.component.ts
+++ my-ng-app/src/app/servers/servers.component.ts
@@ -10,10 +10,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
+
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+  //Beggining the method name with 'on..' make more easy to know that is a event binding, some reaction to user action
 }
