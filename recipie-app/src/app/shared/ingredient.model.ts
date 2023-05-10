924712bbcba11edf7008b9ce12d4b7fa88e009b2
vickats <victoriaktsuha@outlook.com>
2023-05-10 17:18:02 -0300

    Angular Udemy - #56 - 62

---
+++ recipie-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+  // we can declare the properties like this, directly in the constructor parameters, a short-way to do the same thing for a model
+}
